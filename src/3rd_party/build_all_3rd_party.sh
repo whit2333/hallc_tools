7f4f704fc56cb8e20098e0cd2969bd75f53090cb
Whitney Armstrong <warmstrong@anl.gov>
2018-09-23 16:25:36 -0500

    Added script to build all dependencies.
    This is not integrated with the build but could be so in the future.

---
+++ src/3rd_party/build_all_3rd_party.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+prefix=$1
+
+mkdir -p scratch && cd scratch
+
+git clone https://github.com/nlohmann/json.git
+mkdir -p build_json  
+pushd build_json
+cmake ../json/. -DCMAKE_INSTALL_PREFIX=${prefix} \
+   -DBUILD_TESTING=OFF
+make -j4 install
+popd 
+rm -r build_json
+
+git clone https://github.com/fmtlib/fmt.git
+mkdir -p build_fmt
+pushd build_fmt
+cmake ../fmt/. -DCMAKE_INSTALL_PREFIX=${prefix} \
+   -DFMT_TEST=OFF  -DFMT_DOC=OFF -DBUILD_SHARED_LIBS=ON
+make -j4 install
+popd 
+rm -r build_fmt
+
+git clone https://github.com/HowardHinnant/date.git
+mkdir -p build_date
+pushd build_date
+cmake ../date/. -DCMAKE_INSTALL_PREFIX=${prefix} \
+   -DENABLE_DATE_TESTING=OFF -DBUILD_SHARED_LIBS=ON
+make -j4 install
+popd 
+rm -r build_date
+
+git clone https://github.com/ericniebler/range-v3.git
+mkdir -p build_range
+pushd build_range
+cmake ../range-v3/. -DCMAKE_INSTALL_PREFIX=${prefix} \
+   -DRANGE_V3_PERF=OFF -DRANGE_BUILD_CALENDAR_EXAMPLE=OFF -DBUILD_TESTING=OFF -DRANGE_V3_EXAMPLES=OFF -DRANGE_V3_TESTS=OFF -DRANGE_V3_DOCS=OFF -DRANGES_CXX_STD=17
+make -j4 install
+popd 
+rm -r build_range
+
+wget http://bitbucket.org/eigen/eigen/get/3.3.5.tar.gz -O - | tar -xzf -
+mv eigen-eigen* eigen
+mkdir -p build_eigen
+pushd build_eigen
+cmake ../eigen/. -DCMAKE_INSTALL_PREFIX=${prefix} \
+  -DBUILD_TESTING=OFF
+make -j4 install
+pushd ${prefix}/include
+ln -s eigen3/Eigen
+ls
+popd
+popd
+rm -r build_eigen
+
