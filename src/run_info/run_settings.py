8025f9a6eed4854fe97f109355d3511c04f5408d
Whitney Armstrong <warmstrong@anl.gov>
2018-11-03 18:39:42 -0400

    Added run_list in db2

--- src/run_info/run_settings.py
+++ src/run_info/run_settings.py
@@ -1,17 +1,27 @@
-#!/usr/bn/env python
+#!/usr/bin/env python
+import epics
 from epics import caget, caput, camonitor, PV
 import npyscreen
 import datetime
 
-run_number_pv = PV('hcCOINRunNumber')
-
 class TestApp(npyscreen.NPSApp):
+    def onValueChange(self,pvname=None, value=None, host=None, **kws):
+        self.rc.value = value
+
+    def __init__(self):
+        self.run_number_pv     = PV('hcCOINRunNumber')
+        self.run_charge_pv     = PV('hcCOINRunAccumulatedCharge')
+        self.setting_charge_pv = PV('hcRunSettingAccumulatedCharge')
+        self.run_charge_pv.add_callback(self.onValueChange)
+
+
     def main(self):
         # These lines create the form and populate it with widgets.
         # A fairly complex screen in only 8 or so lines of code - a line for each control.
         npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
         F = npyscreen.ActionFormWithMenus(name = "Welcome to Npyscreen",)
-        t = F.add(npyscreen.TitleText, name = "Run Number:",  value=str(int(run_number_pv.get())))
+        self.t = F.add(npyscreen.TitleText, name = "Run Number:",  value=str(int(self.run_number_pv.get())))
+        self.rc = F.add(npyscreen.TitleText, name = "Run Charge:",  value=str(float(self.run_charge_pv.get())))
         fn = F.add(npyscreen.TitleFilename, name = "Filename:")
         dt = F.add(npyscreen.TitleDateCombo, name = "Date:", value=datetime.datetime.now(), use_datetime=True)
         s = F.add(npyscreen.TitleSlider, out_of=12, name = "Slider")
