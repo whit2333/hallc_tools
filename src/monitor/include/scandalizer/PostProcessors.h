4991b1dc711d84a3e742cf55b55b8060e8981ec8
Whitney Armstrong <warmstrong@anl.gov>
2019-01-16 17:09:13 -0600

    new file:   include/scandalizer/PostProcessors.h 	new file:   src/PostProcessors.cxx

---
+++ src/monitor/include/scandalizer/PostProcessors.h
@@ -0,0 +1,97 @@
+#ifndef hallc_scandalizer_PostProcessors_HH
+#define hallc_scandalizer_PostProcessors_HH
+
+#include "THcParmList.h"
+#include "CodaDecoder.h"
+#include "THaEvData.h"
+#include "THaPostProcess.h"
+
+#include <functional>
+#include <iostream>
+
+namespace hallc {
+  namespace scandalizer {
+
+    /** Simple  PostProcess implementation with some lambda hooks.
+     */
+    class SimplePostProcess : public THaPostProcess {
+    public:
+      using Function_t     = std::function<int(const THaEvData*)>;
+      using InitFunction_t = std::function<int()>;
+      Function_t     _event_lambda;
+      InitFunction_t _init_lambda;
+
+    public:
+      SimplePostProcess(Function_t&& f) : 
+        _event_lambda(std::forward<Function_t>(f)), 
+        _init_lambda([](){return 0;}) { }
+
+      SimplePostProcess(InitFunction_t&& initf, Function_t&& f) : 
+        _event_lambda(std::forward<Function_t>(f)), 
+        _init_lambda(std::forward<InitFunction_t>(initf)) { }
+
+      //ofstream   _output_file;
+      //SimplePostProcess()  { }
+      virtual ~SimplePostProcess(){ }
+
+      virtual Int_t Init(const TDatime& ) ;
+      virtual Int_t Process( const THaEvData* evt, const THaRunBase*, Int_t code );
+      virtual Int_t Close();
+
+      ClassDef(SimplePostProcess,1)
+    };
+
+  //SimplePostProcess* pp1 = new SimplePostProcess(
+  //  [&](){
+  //    return 0;
+  //  },
+  //  [&](const THaEvData* evt){
+  //    static int counter = 0;
+  //    static double eff_num             = 0.0000001;
+  //    static double eff_den             = 0.0;
+  //    static int    n_num               = 0;
+  //    static int    n_den               = 0;
+  //    int           shmsDC1Planes_nhits = 0;
+  //    int           shmsDC2Planes_nhits = 0;
+  //    for (int ip = 0; ip < 6; ip++) {
+  //      shmsDC1Planes_nhits += pdc->GetPlane(ip)->GetNHits();
+  //    }
+  //    for (int ip = 6; ip < 12; ip++) {
+  //      shmsDC2Planes_nhits += pdc->GetPlane(ip)->GetNHits();
+  //    }
+  //    bool   shms_DC_too_many_hits = (shmsDC1Planes_nhits > 8) || (shmsDC2Planes_nhits > 8);
+  //    double beta                  = phod->GetBetaNotrk();
+  //    bool   good_beta             = beta > 0.4;
+  //    bool   shms_good_hodoscope   = phod->fGoodScinHits;
+  //    bool   good_ntracks          = (pdc->GetNTracks() > 0);
+  //    bool   good_hgc              = phgcer->GetCerNPE() > 1;
+  //    if ((good_beta && shms_good_hodoscope) && (!shms_DC_too_many_hits) && good_hgc) {
+  //      eff_den = eff_den + 1.0;
+  //      n_den++;
+  //      if (good_ntracks) {
+  //        eff_num = eff_num + 1.0;
+  //        n_num++;
+  //      }
+  //    }
+  //    if ((evt->GetEvNum() > 1200) && (counter > 1000)) {
+  //      std::cout << " efficiency :  " << eff_num / eff_den << "\n";
+  //      //std::cout << " Event : " << evt->GetEvNum() << "  ( " << evt->GetEvType() << ")\n";
+  //      pv_list.Put("hcSHMSTrackingEff",eff_num/eff_den);
+  //      pv_list.Put("hcSHMSTrackingEff:Unc",std::sqrt(double(n_num))/(n_num+n_den+0.0000001));
+  //      eff_num                = 0.000000001;
+  //      eff_den                = 0.0;
+  //      //analyzer->_skip_events = 300;
+  //      counter = 0;
+  //    }
+  //    counter++;
+  //    return 0; 
+  //  });
+  //}
+
+
+}
+}
+
+
+#endif
+
---
+++ src/monitor/src/PostProcessors.cxx
@@ -0,0 +1,31 @@
+#include "scandalizer/PostProcessors.h"
+
+#include "TSystem.h"
+#include "THaEvData.h"
+
+
+namespace hallc {
+  namespace scandalizer {
+
+      Int_t SimplePostProcess::Init(const TDatime& ) {
+        //std::cout << derp;
+        //_output_file.open("derp");
+        return _init_lambda();
+      }
+      Int_t SimplePostProcess::Process( const THaEvData* evt, const THaRunBase*, Int_t code ){ 
+        _event_lambda(evt);
+        //_output_file << " Event : " << evt->GetEvNum() << "  ( " << evt->GetEvType() << ")\n";
+        if( evt->GetEvNum()%100 == 0) {
+          gSystem->ProcessEvents();
+          //_output_file.flush();
+        }
+        return 0;
+      }
+      Int_t SimplePostProcess::Close(){ 
+        std::cout << "close\n";
+        //_output_file.flush();
+        //_output_file.close();
+        return 0; 
+      }
+  }
+}
