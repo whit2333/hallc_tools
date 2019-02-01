df6e079c215a067892b1fd83a17190b322b26d2e
Whitney Armstrong <warmstrong@anl.gov>
2019-01-31 21:32:43 -0600

    damn python clean sucks

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,4 +1,39 @@
 from setuptools import setup
+from setuptools.command import install 
+from distutils.cmd import Command
+import glob
+import os
+import shutil
+
+here = os.getcwd()
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(os.path.normpath(os.path.join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError("%s is not a path inside %s" % (path, here))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
+
+
+
 
 setup(name='hallc',
       version='0.1',
@@ -20,6 +55,5 @@ setup(name='hallc',
           "Operating System :: OS Independent",
           ],
       zip_safe=False,
+      cmdclass = {'clean':CleanCommand}
       )
-
-
